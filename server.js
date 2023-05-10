fbbbc5dfbe2866ec957014513bb007ea208ad98b
Dane Kargis <dane.kargis@gmail.com>
2023-05-09 18:37:29 -0600

    Fixed everything

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/routes.js');
+const HTMLroutes = require('./routes/HTMLroutes.js');
+
+const app = express();
+
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use('/api', api);
+app.use('/',HTMLroutes)
+
+
+
+
+// app.post('/notes', (req, res) => {
+//   console.log(req.body);
+//   res.json(req.body);
+// });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+
